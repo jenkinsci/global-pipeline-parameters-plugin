8b4e39b8c2da079ddd5fec0fdf03647791651235
reffum <reffum@bk.ru>
2024-06-08 23:25:09 +0300

    Suppress NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE
    Jenkins.getInstance() must be not null.

--- src/main/java/io/jenkins/plugins/gparams/Parameters.java
+++ src/main/java/io/jenkins/plugins/gparams/Parameters.java
@@ -1,14 +1,16 @@
 package io.jenkins.plugins.gparams;
 
-import jenkins.model.Jenkins;
-
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.Objects;
+import jenkins.model.Jenkins;
 
 /**
  * Common parameters
  */
+@SuppressFBWarnings("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
 public class Parameters {
-    static final String JenkinsHomeDir = Objects.requireNonNull(Jenkins.getInstanceOrNull()).getRootDir().getAbsolutePath();
+    static final String JenkinsHomeDir =
+            Objects.requireNonNull(Jenkins.getInstanceOrNull()).getRootDir().getAbsolutePath();
     static final String GParamDirectoryName = JenkinsHomeDir + "/gparams/";
 
     // Length of parameter 'name' in gread and gwrite steps
