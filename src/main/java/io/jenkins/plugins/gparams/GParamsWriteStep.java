9ec979580ba569adf509387c15a5ef04c3909d82
reffum <reffum@bk.ru>
2024-06-08 23:24:08 +0300

    Fix spotbug format violation

--- src/main/java/io/jenkins/plugins/gparams/GParamsWriteStep.java
+++ src/main/java/io/jenkins/plugins/gparams/GParamsWriteStep.java
@@ -3,10 +3,6 @@ package io.jenkins.plugins.gparams;
 import hudson.Extension;
 import hudson.model.TaskListener;
 import io.jenkins.cli.shaded.org.apache.commons.lang.StringUtils;
-import org.jenkinsci.plugins.workflow.steps.*;
-import org.kohsuke.stapler.DataBoundConstructor;
-import org.springframework.lang.NonNull;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -16,6 +12,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.Set;
+import org.jenkinsci.plugins.workflow.steps.*;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.springframework.lang.NonNull;
 
 /**
  * Write global parameter that is can be read any Jobs in JENKINS with by key 'name'
