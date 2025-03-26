beca53c53778f7649be6acaeee30a93c19fa5caa
Oleg Gavrilchenko <oleggavrilcenko6@gmail.com>
2025-03-26 22:12:46 +0300

    Fix Jenkins.get() method calling

--- src/main/java/io/jenkins/plugins/gparams/Parameters.java
+++ src/main/java/io/jenkins/plugins/gparams/Parameters.java
@@ -1,16 +1,12 @@
 package io.jenkins.plugins.gparams;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import java.util.Objects;
 import jenkins.model.Jenkins;
 
 /**
  * Common parameters
  */
-@SuppressFBWarnings("NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE")
 public class Parameters {
-    static final String JenkinsHomeDir =
-            Objects.requireNonNull(Jenkins.getInstanceOrNull()).getRootDir().getAbsolutePath();
+    static final String JenkinsHomeDir = Jenkins.get().getRootDir().getAbsolutePath();
     static final String GParamDirectoryName = JenkinsHomeDir + "/gparams/";
 
     // Length of parameter 'name' in gpRead and gpWrite steps
